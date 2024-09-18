cc09594c06c07b81862d3ab52796682cf3616f8a
agpaknazli <agpaknazli19@gmail.com>
2024-09-18 19:54:43 +0200

    navbar.jsx

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,12 +1,23 @@
 import React from "react";
-import{Route,Routes} from "react-router-dom";
-import Main from "../pages/Login";
+import { Route, Routes } from "react-router-dom";
+import Main from "../pages/Main";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
-import 
+import MovieDetail from "../pages/MovieDetail";
+import Navbar from "../components/Navbar";
 
 const AppRouter = () => {
-  return <div>AppRouter</div>;
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/details/:movieId" element={<MovieDetail />} />
+      </Routes>
+    </>
+  );
 };
 
 export default AppRouter;
